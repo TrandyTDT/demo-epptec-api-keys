923a254553578ca78bf2a5d983ddf06d9377687c
Duc Tam Tran <trandy@Ducs-MBP.netis>
2024-03-25 20:54:12 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/ApiKeysDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeysDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKeysDemoApplication.class, args);
+    }
+
+}
